1f45b581b1f58a3f78fa9832eccac552f1ffc99c
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2021-11-23 14:50:48 -0500

    asdf

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -1,64 +1,101 @@
 package zooAnimales;
 
 import java.util.ArrayList;
-import gestion.Zona;
+
+import gestion.*;
 
 public class Animal {
-	private int totalAnimales;
+	private static int totalAnimales;
 	private String nombre;
 	private int edad;
 	private String habitat;
 	private String genero;
-	private ArrayList<Zona> zona;
+	private ArrayList<Zona> zona = new ArrayList<Zona>();
+	
+	public Animal(String nombre, int edad, String habitat, String genero) {
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.setHabitat(habitat);
+		this.setGenero(genero);
+		totalAnimales++;
+	}
 	
 	public Animal() {
-		totalAnimales += 1;
-		zona = new ArrayList<Zona>();
+		totalAnimales++;
 	}
-	public Animal(String nombre,int edad,String habitat,String genero) {
-		this.totalAnimales += 1;
-		this.nombre = nombre;
-		this.edad = edad;
-		this.habitat = habitat;
-		this.genero = genero;
-	}
+	
 	public static String totalPorTipo() {
-		return  "Mamiferos: "+Mamifero.Mamiferos()+"\n"+   
-				"Aves: "+Ave.Aves()+"\n"+
-				"Reptiles: "+Reptil.Reptiles()+"\n"+
-				"Peces: "+Pez.Peces()+"\n"+
-				"Anfibios: "+Anfibio.Anfibios();
-
+		return "Mamiferos: " + Mamifero.cantidadMamiferos()+"\n"+
+				"Aves: " + Ave.cantidadAves()+"\n"+
+				"Reptiles: " + Reptil.cantidadReptiles()+"\n"+
+				"Peces: " + Pez.cantidadPeces()+"\n"+
+				"Anfibios: " + Anfibio.cantidadAnfibios();
 	}
+	
+	@Override
+	public String toString() {
+		String formato, patron;
+		if(zona.size() == 1) {
+			patron = "Mi nombre es %s, tengo una edad de %d, habito en %s y mi genero es %s, la zona en la que me ubico es %s, en el %s";
+			formato = String.format(patron, nombre, edad, habitat, genero, zona.get(0).getNombre(),zona.get(0).getZoo().getNombre());
+			return formato;
+		}else {
+			patron = "Mi nombre es %s, tengo una edad de %d, habito en %s y mi genero es %s";
+			formato = String.format(patron, nombre, edad, habitat, genero);
+			return formato;
+		}
+	}
+	
+	public String movimiento() {
+		return "desplazarse";
+	}
+	
+	// get and set
+	public static int getTotalAnimales() {
+		return totalAnimales;
+	}
+
+	public static void setTotalAnimales(int totalAnimales) {
+		Animal.totalAnimales = totalAnimales;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	public int getEdad() {
-		return edad;
-	}
-	public String getHabitat() {
-		return habitat;
-	}
-	public String getGenero() {
-		return genero;
-		}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	public int getEdad() {
+		return edad;
+	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
 	public void setHabitat(String habitat) {
 		this.habitat = habitat;
 	}
+
+	public String getGenero() {
+		return genero;
+	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
-	@Override
-	public String toString() {
-		return "Mi nombre es "+nombre+", tengo una edad de "+edad+", habito en "+habitat+" y mi genero es "+genero;
+
+	public ArrayList<Zona> getZona() {
+		return zona;
 	}
-	public String movimiento() {
-		return "desplazarse";
+
+	public void setZona(Zona zona) {
+		this.zona.add(zona);
 	}
 }
